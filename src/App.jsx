e632465ca9c7a7a97bacba19cd0e4f2862a53edb
GiorgiQashiashvili <qashiashviligiorgi9@gmail.com>
2025-10-16 02:16:18 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { Routes, Route, HashRouter } from "react-router-dom"
 import AccountLog from "../src/components/pages/AccountLog"
 import Home from "../src/components/pages/Home"
 import AccountSignUP from "../src/components/pages/AccountSignUp"
 
 function App() {
   return (
-    <BrowserRouter basename="/react-bike-store">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<AccountLog />} />
         <Route path="/signup" element={<AccountSignUP />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
